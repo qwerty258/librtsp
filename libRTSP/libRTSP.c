4d0900a30caa7c0bdac07c87c85c85bb5a396df3
qwerty258 <zyf26256@gmail.com>
2015-09-30 10:08:13 +0800

    add code to API

--- libRTSP/libRTSP.c
+++ libRTSP/libRTSP.c
@@ -1,26 +1,40 @@
 #include "libRTSP.h"
 #include <limits.h>
-#include <SDKDDKVer.h>
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files:
-#include <windows.h>
+#include <SDKDDKVer.h>
+#include <WinSock2.h>
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#include <Windows.h>
 
 LIBRTSP_API unsigned int initializeDLLAsRTSPClient(void)
 {
+    WSADATA wsaData;
+    int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if(0 != result)
+    {
+        return UINT_MAX;
+    }
     return 0;
 }
 
 LIBRTSP_API unsigned int freeRTSPClinetDLL(void)
 {
+    int result = WSACleanup();
+    if(0 != result)
+    {
+        return UINT_MAX;
+    }
     return 0;
 }
 
 LIBRTSP_API unsigned int initializeDLLAsRTSPServer(void)
 {
-    return 0;
+    unsigned int result = initializeDLLAsRTSPClient();
+    return result;
 }
 
 LIBRTSP_API unsigned int freeRTSPServerDLL(void)
 {
-    return 0;
+    unsigned int result = freeRTSPClinetDLL();
+    return result;
 }
