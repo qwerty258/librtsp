39f06a3ee43a03cd0978ec0868ee5f6a52c628af
qwerty258 <zyf26256@gmail.com>
2015-09-30 11:42:11 +0800

    fix message variable redefinition

--- libRTSP/handleError.c
+++ libRTSP/handleError.c
@@ -3,11 +3,11 @@
 #include <Windows.h>
 void handleErrorForLibRTSP(char* message, char* sourceFile, int line, int errorCode)
 {
-    char message[2048];
-    int result = sprintf_s(message, "message: %s\r\nfile: %s\r\nline: %d\r\nerror code: %d", message, sourceFile, line, errorCode);
+    char errorMessage[2048];
+    int result = sprintf_s(errorMessage, "message: %s\r\nfile: %s\r\nline: %d\r\nerror code: %d", message, sourceFile, line, errorCode);
     if(-1 == result)
     {
         return;
     }
-    MessageBoxA(NULL, message, "ERROR", MB_OK);
+    MessageBoxA(NULL, errorMessage, "ERROR", MB_OK);
 }
