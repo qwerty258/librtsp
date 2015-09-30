f15fc49dd9f93f6834ea1ccaa8612e1420d9feb5
qwerty258 <zyf26256@gmail.com>
2015-09-30 13:32:42 +0800

    add test code

--- generalTest/generalTest.cpp
+++ generalTest/generalTest.cpp
@@ -1,5 +1,18 @@
+#include <libRTSP.h>
+
 int main(int argc, char* argv[])
 {
+    unsigned int result = initializeDLLAsRTSPClient();
+
+    RTSPClientHandle handle = getNewRTSPHandle();
+
+    setRTSPURI(handle, "rtsp://192.168.10.222:554/Streaming/Channels/1?transportmode=unicast");
+
+    setRTSPProtocol(handle, RTSPUsingTCP);
+
+    freeRTSPHandle(&handle);
+
+    result = freeRTSPClinetDLL();
     return 0;
 }
 
