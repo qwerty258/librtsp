b2fec54d8d0b6835a10973c0d2e456d336b76f84
qwerty258 <zyf26256@gmail.com>
2015-10-02 17:17:51 +0800

    put codes into if

--- libRTSP/RTSPURIParser.c
+++ libRTSP/RTSPURIParser.c
@@ -18,21 +18,21 @@ unsigned int parseRTSPURI(char* URI, unsigned long* IPv4, unsigned short* port)
         if(0 == strncmp("rtsp://", URITemp, strnlen("rtsp://", 20)));
         {
             currnetPosition = URITemp + strnlen("rtsp://", 20);
-        }
-        tempPosition = strstr(currnetPosition, ":");
-        if(NULL != tempPosition)
-        {
-            *port = htons((unsigned short)strtoul(tempPosition + 1, &end, 10));
-        }
-        memcpy_s(IPString, 20, currnetPosition, tempPosition - currnetPosition);
-        result = inet_pton(AF_INET, (char*)IPString, IPv4);
-        if(1 != result)
-        {
-            parseResult = UINT_MAX;
-        }
-        else
-        {
-            parseResult = 0;
+            tempPosition = strstr(currnetPosition, ":");
+            if(NULL != tempPosition)
+            {
+                *port = htons((unsigned short)strtoul(tempPosition + 1, &end, 10));
+            }
+            memcpy_s(IPString, 20, currnetPosition, tempPosition - currnetPosition);
+            result = inet_pton(AF_INET, (char*)IPString, IPv4);
+            if(1 != result)
+            {
+                parseResult = UINT_MAX;
+            }
+            else
+            {
+                parseResult = 0;
+            }
         }
         free(URITemp);
     }
