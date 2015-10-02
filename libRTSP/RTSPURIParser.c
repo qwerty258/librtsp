63d42cfec5cbe809e08acd11d18b2109a2e91013
qwerty258 <zyf26256@gmail.com>
2015-10-02 16:50:29 +0800

    add URI parser

---
+++ libRTSP/RTSPURIParser.c
@@ -0,0 +1,40 @@
+#include "RTSPURIParser.h"
+#include <WS2tcpip.h>
+#include <WinSock2.h>
+
+unsigned int parseRTSPURI(char* URI, unsigned long* IPv4, unsigned short* port)
+{
+    char* URITemp = _strdup(URI);
+    char* IPString[21];
+    char* currnetPosition;
+    char* tempPosition;
+    char* end;
+    int result;
+    unsigned int parseResult = UINT_MAX;
+    if(NULL != URITemp)
+    {
+        memset(IPString, 0x0, 21);
+        *port = htons(554);
+        if(0 == strncmp("rtsp://", URITemp, strnlen("rtsp://", 20)));
+        {
+            currnetPosition = URITemp + strnlen("rtsp://", 20);
+        }
+        tempPosition = strstr(currnetPosition, ":");
+        if(NULL != tempPosition)
+        {
+            *port = (unsigned short)strtoul(tempPosition + 1, &end, 10);
+        }
+        memcpy_s(IPString, 20, currnetPosition, tempPosition - currnetPosition);
+        result = inet_pton(AF_INET, (char*)IPString, IPv4);
+        if(1 != result)
+        {
+            parseResult = UINT_MAX;
+        }
+        else
+        {
+            parseResult = 0;
+        }
+        free(URITemp);
+    }
+    return parseResult;
+}
---
+++ libRTSP/RTSPURIParser.h
@@ -0,0 +1,3 @@
+#pragma once
+
+unsigned int parseRTSPURI(char* URI, unsigned long* IPv4, unsigned short* port);
